3031caf9c9dc3cec9a1e6fb538dc815b4b7d27a4
Kelly Cruz <kelly@kellys-MacBook-Pro-2019.local>
2023-01-18 17:26:04 -0300

    first commit

---
+++ app.rb
@@ -0,0 +1,19 @@
+require_relative 'mega_anfitriao'
+
+if __FILE__ == $0
+  mg = MegaAnfitriao.new
+  mg.diz_ola
+  mg.diz_adeus
+
+  mg.nomes = "Diogo"
+  mg.diz_ola
+  mg.diz_adeus
+
+  mg.nomes = ["Alberto", "Beatriz", "Carlos", "David", "Ernesto"]
+  mg.diz_ola
+  mg.diz_adeus
+
+  mg.nomes = nil?
+  mg.diz_ola
+  mg.diz_adeus
+end
